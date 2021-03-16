07c084ead15b80db05ab21dcf88c74ee378d6eb3
yatsun <yatsunlam25@gmail.com>
2021-03-15 22:51:41 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,43 @@
+require 'pry'
+class CashRegister
 
+    attr_accessor :total
+
+    def initialize(total=0)
+        @total = total
+        # @discount = discount
+        
+    end
+
+    # def discount=(discount)
+    #     @discount = discount
+    # end
+
+    def discount
+        @total
+    end
+
+    # def total=(total)
+    #    @total = total
+    # end
+
+    # def total
+    #     @total
+    # end
+
+    def add_item(title, price, quantity=1)
+        self.total += quantity*price
+    end
+
+    def apply_discount
+        # binding.pry
+        # total = 800.0 - @total
+        # discount = 800.0 / @total
+        # @total *= discount
+        # @total.reduce(20)
+        @total = @total * (800.0 / @total)
+        "After the discount, the total comes to $#{@total.to_i}."
+        
+        
+    end
+end
